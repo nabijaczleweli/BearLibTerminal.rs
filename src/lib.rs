73bf5af050ca0f53b49b0cff278f1bb7fc4f5645
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-24 16:20:20 +0100

    Cannot open terminal on CI, as there's no graphical env. there

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,13 @@
 //!
 //! # Examples
 //!
-//! ```
+//! ```ignore
 //! use bear_lib_terminal::terminal;
 //!
 //! terminal::open("Test", 80, 30);
 //! terminal::print_xy(0, 0, "[color=red]asdf[bkcolor=blue]asdf");
 //! terminal::refresh();
-//! # /* Don't wait while testing
 //! let _ = terminal::wait_event();
-//! # */
 //! terminal::close();
 //! ```
 
