19b64ed86cc43f21841e68083dd8c0346a2d0917
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-27 20:40:50 +0100

    Implement TrueType fonts. Add docs for Font properties

--- src/terminal/config/font.rs
+++ src/terminal/config/font.rs
@@ -4,11 +4,11 @@ use geometry::Size;
 use terminal::config::{ConfigPart, escape_config_string};
 
 
-
 pub fn bitmap<T: AsRef<Path>>(origin: Origin, path: T) -> BitmapFont {
 	BitmapFont{
 		origin: origin,
 		path: path.as_ref().to_str().unwrap().to_string(),
+		size: None,
 		resize: None,
 		resize_filter: None,
 		resize_mode: None,
@@ -19,7 +19,18 @@ pub fn bitmap<T: AsRef<Path>>(origin: Origin, path: T) -> BitmapFont {
 	}
 }
 
-pub fn true_type<T: AsRef<Path>>(origin: Origin, path: T, tile_size: Size) {}
+pub fn true_type<T: AsRef<Path>>(origin: Origin, path: T, tile_size: Size) -> TrueTypeFont {
+	TrueTypeFont{
+		origin: origin,
+		path: path.as_ref().to_str().unwrap().to_string(),
+		size: tile_size,
+		size_reference: None,
+		mode: None,
+		codepage: None,
+		align: None,
+		spacing: None,
+	}
+}
 
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
@@ -70,6 +81,7 @@ pub enum Align {
 pub struct BitmapFont {
 	origin       : Origin,
 	path         : String,
+	size         : Option<Size>,
 	resize       : Option<Size>,
 	resize_filter: Option<ResizeFilter>,
 	resize_mode  : Option<ResizeMode>,
@@ -80,15 +92,71 @@ pub struct BitmapFont {
 //	transparent  : Option<Background>,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct TrueTypeFont {
+	origin        : Origin,
+	path          : String,
+	size          : Size,
+	size_reference: Option<char>,
+	mode          : Option<RasterizationMode>,
+	codepage      : Option<String>,
+	align         : Option<Align>,
+	spacing       : Option<Size>,
+}
 
+
+/// For all functions consult the corresponding attributes in
+/// [the official docs](http://foo.wyrd.name/en:bearlibterminal:reference:configuration#font_and_tileset_management).
 impl BitmapFont {
-	builder!(resize, Size);
-	builder!(resize_filter, ResizeFilter);
-	builder!(resize_mode, ResizeMode);
-	builder!(raw_size, Size);
-	builder!(codepage, String);
-	builder!(align, Align);
-	builder!(spacing, Size);
+	/// The size of a single tile in the tileset.
+	pub fn size         (mut self, size: Size)                  -> Self {self.size          = Some(size)         ; self}
+	/// The size to resize the image to.
+	pub fn resize       (mut self, resize: Size)                -> Self {self.resize        = Some(resize)       ; self}
+	/// How to resize the image.
+	///
+	/// Default: `ResizeFilter::Bilinear`.
+	pub fn resize_filter(mut self, resize_filter: ResizeFilter) -> Self {self.resize_filter = Some(resize_filter); self}
+	/// Resize aspect method.
+	///
+	/// Default: `ResizeMode::Stretch`.
+	pub fn resize_mode  (mut self, resize_mode: ResizeMode)     -> Self {self.resize_mode   = Some(resize_mode)  ; self}
+	/// Raw memory size, `size` if not specified.
+	pub fn raw_size     (mut self, raw_size: Size)              -> Self {self.raw_size      = Some(raw_size)     ; self}
+	/// Tileset's codepage.
+	///
+	/// Default: `"ascii"`.
+	pub fn codepage     (mut self, codepage: String)            -> Self {self.codepage      = Some(codepage)     ; self}
+	/// How tiles are to be aligned.
+	///
+	/// Default: `Align::Center`.
+	pub fn align        (mut self, align: Align)                -> Self {self.align         = Some(align)        ; self}
+	/// Tile alignment area \[cells\].
+	///
+	/// Default: `1x1`.
+	pub fn spacing      (mut self, spacing: Size)               -> Self {self.spacing       = Some(spacing)      ; self}
+}
+
+/// For all functions consult the corresponding attributes in
+/// [the official docs](http://foo.wyrd.name/en:bearlibterminal:reference:configuration#font_and_tileset_management).
+impl TrueTypeFont {
+	/// Character used for size probing.
+	///
+	/// Default: `'@'`.
+	pub fn size_reference(mut self, size_reference: char)    -> Self {self.size_reference = Some(size_reference); self}
+	/// Rasterization mode. Note: `RasterizationMode::Lcd` forces opaque black background.
+	///
+	/// Default: `RasterizationMode::Normal`.
+	pub fn mode          (mut self, mode: RasterizationMode) -> Self {self.mode           = Some(mode)          ; self}
+	/// Reverse codepage for loading symbols.
+	pub fn codepage      (mut self, codepage: String)        -> Self {self.codepage       = Some(codepage)      ; self}
+	/// How tiles are to be aligned.
+	///
+	/// Default: `Align::Center`.
+	pub fn align         (mut self, align: Align)            -> Self {self.align          = Some(align)         ; self}
+	/// Tile alignment area \[cells\].
+	///
+	/// Default: `1x1`.
+	pub fn spacing       (mut self, spacing: Size)           -> Self {self.spacing        = Some(spacing)       ; self}
 }
 
 
@@ -113,7 +181,34 @@ impl ConfigPart for BitmapFont {
 			},
 			match self.codepage {
 				None               => "".to_string(),
-				Some(ref codepage) => format!(", codepage={}", codepage),
+				Some(ref codepage) => format!(", codepage={}", escape_config_string(codepage)),
+			},
+			match self.align {
+				None            => "".to_string(),
+				Some(ref align) => format!(", align={}", align),
+			},
+			match self.spacing {
+				None              => "".to_string(),
+				Some(ref spacing) => format!(", spacing={}", spacing),
+			}
+		)
+	}
+}
+
+impl ConfigPart for TrueTypeFont {
+	fn to_config_str(&self) -> String {
+		format!("{}: {}, size={}{}{}{}{}{};", self.origin, escape_config_string(&self.path), self.size,
+			match self.size_reference {
+				None                     => "".to_string(),
+				Some(ref size_reference) => format!(", size-reference=0x{:X}", *size_reference as i32),
+			},
+			match self.mode {
+				None           => "".to_string(),
+				Some(ref mode) => format!(", mode={}", mode),
+			},
+			match self.codepage {
+				None               => "".to_string(),
+				Some(ref codepage) => format!(", codepage={}", escape_config_string(codepage)),
 			},
 			match self.align {
 				None            => "".to_string(),
@@ -177,14 +272,4 @@ impl fmt::Display for Align {
 			&Align::BottomRight => formatter.write_str("bottom-right"),
 		}
 	}
-}
-
-
-macro_rules! builder {
-	($name:ident, $argt:ty) => {
-		pub fn $name(mut self, $name: $argt) -> Self {
-			self.$name = Some($name);
-			self
-		}
-	};
 }
